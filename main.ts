a52aaf40109b0f6b5fd6deb0d7098953a5255302
ElGoha <107138039+ElGoha@users.noreply.github.com>
2022-07-13 11:36:18 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # #
+            . . # . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . # # # .
+            # # . . #
+            # . # . #
+            # . . # #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    }
+})
